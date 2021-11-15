98529c6e23ae1b6e9625b320339d270a0969e3ae
Ron Almog <ron8132@gmail.com>
2021-11-15 10:21:05 -0500

    add reducers, connect to app, install devtools

---
+++ src/redux/reducers/productReducer.js
@@ -0,0 +1,14 @@
+import { ActionTypes } from "../constants/action-types";
+
+const initialState = {
+  products: [{ id: 1, title: "test tiele", category: "prog" }],
+};
+export const productReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ActionTypes.SET_PRODUCTS:
+      return state;
+
+    default:
+      return state;
+  }
+};
